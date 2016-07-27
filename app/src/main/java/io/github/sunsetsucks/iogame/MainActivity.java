7baeacad2c6a92d81b7c858254be0c678d6a7ce7
Elaine Yang <elaineyangx.gmail.com>
2016-07-27 13:10:45 -0700

    updated mainactivity

--- app/src/main/java/io/github/sunsetsucks/iogame/MainActivity.java
+++ app/src/main/java/io/github/sunsetsucks/iogame/MainActivity.java
@@ -2,7 +2,7 @@ package io.github.sunsetsucks.iogame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-.
+import android.view.View;
 
 import io.github.sunsetsucks.iogame.view.IOGameGLSurfaceView;
 
@@ -17,5 +17,21 @@ public class MainActivity extends AppCompatActivity
 
         glView = new IOGameGLSurfaceView(this);
         setContentView(glView);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        glView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && glView.getVisibility() == View.GONE) {
+            glView.setVisibility(View.VISIBLE);
+        }
     }
 }
