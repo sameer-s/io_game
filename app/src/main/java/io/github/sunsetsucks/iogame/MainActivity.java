17b6e01c496751eebd81baceadad4b1139afc6df
Sameer Suri <sameer.suri0@gmail.com>
2016-08-01 14:57:30 -0700

    wifidirect
    Signed-off-by: Sameer Suri <sameer.suri0@gmail.com>

--- app/src/main/java/io/github/sunsetsucks/iogame/MainActivity.java
+++ app/src/main/java/io/github/sunsetsucks/iogame/MainActivity.java
@@ -1,23 +1,38 @@
 package io.github.sunsetsucks.iogame;
 
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-import java.io.IOException;
-
-import static io.github.sunsetsucks.iogame.Util.toast;
-
-import io.github.sunsetsucks.iogame.network.GameConnection;
+import io.github.sunsetsucks.iogame.network.DeviceListFragment;
+import io.github.sunsetsucks.iogame.network.WiFiDirectBroadcastReceiver;
+import io.github.sunsetsucks.iogame.network.WifiP2pActivity;
 import io.github.sunsetsucks.iogame.view.IOGameGLSurfaceView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements WifiP2pActivity, DeviceListFragment.DeviceActionListener
 {
     private IOGameGLSurfaceView glView;
-    private Thread networkThread;
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+    private BroadcastReceiver receiver;
+    private IntentFilter intentFilter;
+
+    private boolean isWifiP2pEnabled = false;
+    private boolean retryChannel = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,68 +42,40 @@ public class MainActivity extends AppCompatActivity
         Util.context = this;
 
         glView = new IOGameGLSurfaceView(this);
-        setContentView(glView);
+//        setContentView(glView);
+        setContentView(R.layout.main);
 
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
-//        setContentView(R.layout.activity_main);
-
-        networkThread = new Thread(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                Looper.prepare();
-
-                GameConnection conn = new GameConnection(new Handler()
-                {
-                    @Override
-                    public void handleMessage(Message msg)
-                    {
-                        String data = msg.getData().getString("data");
-                        toast("Message: %s", data);
-                    }
-                });
-
-                Util.connection = conn;
-
-                //noinspection StatementWithEmptyBody
-                while(!conn.isRegistered());
-
-                conn.discoverServices();
-                conn.runServerLoop();
-            }
-        });
-
-        networkThread.start();
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
     }
 
     @Override
-    public void onPause()
+    protected void onResume()
+    {
+        super.onResume();
+
+        receiver = new WiFiDirectBroadcastReceiver(manager, channel);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause()
     {
         super.onPause();
-        if(glView != null) glView.setVisibility(View.GONE);
+        if (glView != null) glView.setVisibility(View.GONE);
+        unregisterReceiver(receiver);
     }
 
     @Override
     public void onDestroy()
     {
         super.onDestroy();
-
-        if(isFinishing())
-        {
-            try
-            {
-                Util.connection.closeAllChannels();
-            } catch (IOException e)
-            {
-                toast("Error in closing network channels");
-            }
-
-            Util.connection.deregisterService();
-            Util.connection = null;
-
-            networkThread.interrupt();
-        }
     }
 
     @Override
@@ -101,4 +88,188 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.atn_direct_enable:
+                if (manager != null && channel != null)
+                {
+
+                    // Since this is the system wireless settings activity, it's
+                    // not going to send us a result. We will be notified by
+                    // WiFiDeviceBroadcastReceiver instead.
+
+                    startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+                } else
+                {
+                    Log.e("wifidirect", "channel or manager is null");
+                }
+                return true;
+
+            case R.id.atn_direct_discover:
+                if (!isWifiP2pEnabled)
+                {
+                    Toast.makeText(MainActivity.this, R.string.p2p_off_warning,
+                            Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                final DeviceListFragment fragment = (DeviceListFragment) getFragmentManager()
+                        .findFragmentById(R.id.frag_list);
+                fragment.onInitiateDiscovery();
+                manager.discoverPeers(channel, new WifiP2pManager.ActionListener()
+                {
+
+                    @Override
+                    public void onSuccess()
+                    {
+                        Toast.makeText(MainActivity.this, "Discovery Initiated",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(int reasonCode)
+                    {
+                        Toast.makeText(MainActivity.this, "Discovery Failed : " + reasonCode,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onChannelDisconnected()
+    {
+        // we will try once more
+        if (manager != null && !retryChannel)
+        {
+            Toast.makeText(this, "Channel lost. Trying again", Toast.LENGTH_LONG).show();
+            resetData();
+            retryChannel = true;
+            manager.initialize(this, getMainLooper(), this);
+        } else
+        {
+            Toast.makeText(this,
+                    "Severe! Channel is probably lost premanently. Try Disable/Re-Enable P2P.",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void itemSelected(WifiP2pDevice device)
+    {
+        WifiP2pConfig config = new WifiP2pConfig();
+        config.deviceAddress = device.deviceAddress;
+        config.wps.setup = WpsInfo.PBC;
+        connect(config);
+    }
+
+    @Override
+    public void cancelDisconnect()
+    {
+        if (manager != null)
+        {
+            final DeviceListFragment fragment = (DeviceListFragment) getFragmentManager()
+                    .findFragmentById(R.id.frag_list);
+            if (fragment.getDevice() == null
+                    || fragment.getDevice().status == WifiP2pDevice.CONNECTED)
+            {
+                disconnect();
+            } else if (fragment.getDevice().status == WifiP2pDevice.AVAILABLE
+                    || fragment.getDevice().status == WifiP2pDevice.INVITED)
+            {
+
+                manager.cancelConnect(channel, new WifiP2pManager.ActionListener()
+                {
+
+                    @Override
+                    public void onSuccess()
+                    {
+                        Toast.makeText(MainActivity.this, "Aborting connection",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(int reasonCode)
+                    {
+                        Toast.makeText(MainActivity.this,
+                                "Connect abort request failed. Reason Code: " + reasonCode,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+
+    }
+
+    @Override
+    public void connect(WifiP2pConfig config)
+    {
+        manager.connect(channel, config, new WifiP2pManager.ActionListener()
+        {
+
+            @Override
+            public void onSuccess()
+            {
+                // WiFiDirectBroadcastReceiver will notify us. Ignore for now.
+            }
+
+            @Override
+            public void onFailure(int reason)
+            {
+                Toast.makeText(MainActivity.this, "Connect failed. Retry.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void disconnect()
+    {
+        manager.removeGroup(channel, new WifiP2pManager.ActionListener()
+        {
+
+            @Override
+            public void onFailure(int reasonCode)
+            {
+                Log.d("wifidirect", "Disconnect failed. Reason :" + reasonCode);
+            }
+
+            @Override
+            public void onSuccess()
+            {
+
+            }
+
+        });
+    }
+
+    @Override
+    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled)
+    {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    }
+
+    @Override
+    public void resetData()
+    {
+        DeviceListFragment fragmentList = (DeviceListFragment) getFragmentManager()
+                .findFragmentById(R.id.frag_list);
+        if (fragmentList != null)
+        {
+            fragmentList.clearPeers();
+        }
+    }
 }
--- app/src/main/java/io/github/sunsetsucks/iogame/Util.java
+++ app/src/main/java/io/github/sunsetsucks/iogame/Util.java
@@ -1,11 +1,11 @@
 package io.github.sunsetsucks.iogame;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.util.Log;
 import android.widget.Toast;
-
-import io.github.sunsetsucks.iogame.network.GameConnection;
 
 /**
  * Created by Sameer on 2016-07-27.
@@ -13,14 +13,10 @@ import io.github.sunsetsucks.iogame.network.GameConnection;
 public class Util
 {
     public static Context context = null;
-    public static GameConnection connection = null;
 
     public static void toast(final int length, final Object text, final Object... params)
     {
-        if(!(context instanceof Activity))
-        {
-            throw new IllegalStateException("Util.context must be an Activity to create toast");
-        }
+        assertActivity("create toast");
 
         ((Activity) context).runOnUiThread(new Runnable()
         {
@@ -34,8 +30,26 @@ public class Util
         });
     }
 
+    private static void assertActivity(String text)
+    {
+        if(!(context instanceof Activity))
+        {
+            throw new IllegalStateException("Util.context must be an Activity to" + text);
+        }
+    }
+
     public static void toast(final Object text, final Object... params)
     {
         toast(Toast.LENGTH_SHORT, text, params);
+    }
+
+    public static void alert(String title, String text, DialogInterface.OnClickListener listener)
+    {
+        new AlertDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(text)
+                .setPositiveButton("OK", listener)
+                .create()
+                .show();
     }
 }
